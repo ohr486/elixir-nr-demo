523fd3e5ddc88ad6e6f1468bb4ec99bb3816b9ff
ohr486 <ohr486@gmail.com>
2023-12-20 14:23:59 +0900

    add base codes

---
+++ config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :demo, Demo.Repo,
+  database: Path.expand("../demo_test.db", Path.dirname(__ENV__.file)),
+  pool_size: 5,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :demo, DemoWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "8HZMYPP7xgHNIXzO6dXQn+QR0Vuz9Q4KsTDuFbq6ZwPHP1l4Jo6K35IWygOVp6Jl",
+  server: false
+
+# In test we don't send emails.
+config :demo, Demo.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
